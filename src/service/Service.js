3af3bc87a3cd9de78b3274f5f7db72c079f29ab9
Ng Han Inn <nghaninn@gmail.com>
2022-10-09 18:18:56 +0800

    Update prod url

--- src/service/Service.js
+++ src/service/Service.js
@@ -1,2 +1,3 @@
-export const url = 'http://localhost:8080'
+// export const url = 'http://localhost:8080'
+export const url = 'https://datwyler-assignment-fqiihixc7a-de.a.run.app'
 export const token = localStorage.getItem('account') ? JSON.parse(localStorage.getItem("account"))?.jwtToken : ''
